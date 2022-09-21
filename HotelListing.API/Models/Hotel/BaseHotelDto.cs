041cf0a85d6211c129e5d38868bc8465a7eb9bcb
kostya-over <kostaover940@gmail.com>
2022-09-21 12:59:32 +0300

    Added Hotels API Endpoint

---
+++ HotelListing.API/Models/Hotel/BaseHotelDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
+namespace HotelListing.API.Models.Hotel;
+
+public abstract class BaseHotelDto
+{
+    [Required]
+    public string Name { get; set; }
+    [Required]
+    public string Address { get; set; }
+    
+    public double? Rating { get; set; }
+    [Required]
+    [Range(1, Int32.MaxValue)]
+    public  int CountryId { get; set; }
+}
